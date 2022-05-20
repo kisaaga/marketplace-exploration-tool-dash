bddaa1d83f3b6f69dc9501564d88fe0fe6f6ef0e
Abidin Gökçekaya <agokcekaya@sabanciuniv.edu>
2022-05-20 15:12:28 +0300

    Data updated. Modals are adjusted for repricing and demo.

--- pages/inventory.py
+++ pages/inventory.py
@@ -1,3 +1,4 @@
+import dash
 from dash import Dash, dash_table, dcc, html, Input, Output, callback, State
 import pandas as pd
 import dash_bootstrap_components as dbc
@@ -6,18 +7,18 @@ df = pd.read_csv('data/amazon_iphone13_phone_cases_25_1_2022.csv')
 
 modal_1 = dbc.Modal(
     [
-        dbc.ModalHeader(dbc.ModalTitle("Modal 1")),
-        dbc.ModalBody(children=[], id="first_modal"),
+        dbc.ModalHeader(dbc.ModalTitle("Reprice")),
+        dbc.ModalBody(children=[], id="reprice_body"),
         dbc.ModalFooter(
             dbc.Button(
-                "Open Modal 2",
-                id="open-toggle-modal-2",
+                "Conduct a demo",
+                id="open_demo_button",
                 className="ms-auto",
                 n_clicks=0,
             )
         ),
     ],
-    id="toggle-modal-1",
+    id="reprice_modal",
     size="lg",
     is_open=False,
 )
@@ -26,16 +27,8 @@ modal_2 = dbc.Modal(
     [
         dbc.ModalHeader(dbc.ModalTitle("Modal 2")),
         dbc.ModalBody("This is the second modal"),
-        dbc.ModalFooter(
-            dbc.Button(
-                "Back to Modal 1",
-                id="open-toggle-modal-1",
-                className="ms-auto",
-                n_clicks=0,
-            )
-        ),
     ],
-    id="toggle-modal-2",
+    id="demo_modal",
     size="lg",
     is_open=False,
 )
@@ -67,9 +60,16 @@ layout = html.Div([
                         page_current=0,
                         page_size=10,
                     ),
-                    dbc.Button('Reprice', id='open-toggle-modal', n_clicks=0),
+                    dbc.Button('Reprice', id='reprice-button', n_clicks=0),
                     modal_1,
                     modal_2,
+                    dbc.Alert(
+                        "Please select a product!",
+                        id="reprice_alert",
+                        dismissable=True,
+                        is_open=False,
+                        color="danger"
+                    ),
                 ]
             )
         )
@@ -78,41 +78,50 @@ layout = html.Div([
 
 
 @callback(
-    Output("toggle-modal-1", "is_open"),
     [
-        Input("open-toggle-modal", "n_clicks"),
-        Input("open-toggle-modal-1", "n_clicks"),
-        Input("open-toggle-modal-2", "n_clicks"),
+        Output("reprice_modal", "is_open"),
+        Output("reprice_alert", "is_open"),
     ],
-    State("toggle-modal-1", "is_open"),
+    [
+        Input("reprice-button", "n_clicks"),
+        Input("open_demo_button", "n_clicks"),
+    ],
+    [
+        State("reprice_modal", "is_open"),
+        State("datatable", "selected_rows"),
+    ]
 )
-def toggle_modal_1(n0, n1, n2, is_open):
-    if n0 or n1 or n2:
-        return not is_open
-    return is_open
+def toggle_reprice_modal(n0, n1, is_open_modal, selected_rows):
+    if len(selected_rows) > 0:
+        if n0 or n1:
+            return (not is_open_modal), False
+    elif n0 != 0:
+        return dash.no_update, True
+    else:
+        return False, False
 
 
 @callback(
-    Output("first_modal", "children"),
-    Input("open-toggle-modal", "n_clicks"),
+    Output("reprice_body", "children"),
+    Input("reprice-button", "n_clicks"),
     State("datatable", "selected_rows")
 )
 def reprice(n_click, selected_rows):
-    row_index = selected_rows[0]
-    print(df.loc[row_index]['Price'])
-    if n_click:
-        return df.loc[row_index]['Price']
+    if len(selected_rows) > 0:
+        row_index = selected_rows[0]
+        print(df.loc[row_index]['Price'])
+        if n_click:
+            return df.loc[row_index]['Price']
+    else:
+        print("No rows selected.")
 
 
 @callback(
-    Output("toggle-modal-2", "is_open"),
-    [
-        Input("open-toggle-modal-2", "n_clicks"),
-        Input("open-toggle-modal-1", "n_clicks"),
-    ],
-    [State("toggle-modal-2", "is_open")],
+    Output("demo_modal", "is_open"),
+    Input("open_demo_button", "n_clicks"),
+    [State("demo_modal", "is_open")],
 )
-def toggle_modal_2(n2, n1, is_open):
-    if n1 or n2:
+def toggle_demo_modal(n1, is_open):
+    if n1:
         return not is_open
     return is_open
